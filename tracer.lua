9cec3179e0c18b919bc519b5d6602072efeb9125
PoptartNoahh <89370441+PoptartNoahh@users.noreply.github.com>
2021-09-02 01:24:26 -0400

    Update tracer.lua

--- tracer.lua
+++ tracer.lua
@@ -1,6 +1,5 @@
 --[[
 BLOXPT
-
 By Noah E.
 ]]
 
@@ -9,7 +8,13 @@ local randVec = function() return Vector3.new(math.random() - .5, math.random() 
 local sRGB = Vector3.new(0.2126, 0.7152, 0.0722)
 local bloxpt = script.Parent
 local params, util = require(bloxpt.Params), require(bloxpt.Util)
-local tracer = { x = nil, n = nil, d = nil, albedo = nil, e = nil, object = nil, depth = 0 }
+
+local tracer = {}
+tracer.__index = tracer
+function tracer.new()
+	local self = {x = nil, n = nil, d = nil, albedo = nil, e = nil, object = nil}
+	return setmetatable(tracer, self)
+end
 function tracer:sphereCoordinates(pos)
 	local long = math.atan2(pos.Z, pos.X)
 	local lat = math.asin(pos.Y / params.hdri_radius)
@@ -21,7 +26,7 @@ end
 function tracer:hemisphere(d, n)
 	local phi, r2 = tau * math.random(), math.random()
 	local sint = math.sqrt(r2)
-	local w = n:Dot(d) < 0 and n or n * -1
+	local w = n:Dot(d) < 0 and n or -n
 	local u = math.abs(w.x) > 0.1 and up:Cross(w) or right:Cross(w)
 	local v = w:Cross(u)
 	return u * math.cos(phi) * sint + v * math.sin(phi) * sint + w * math.sqrt(1 - r2)
@@ -38,14 +43,13 @@ function tracer:fresnel(cosi, eta)
 	return 1
 end
 function tracer:refract(d, n, cosi, ior)
-	local etai, etat = 1, ior ^ 2
+	local eta = ior ^ -2
 	if cosi < 0 then
 		cosi = -cosi 
 	else  
-		etai, etat = etat, etai
+		eta ^= -1
 		n = -n
 	end
-	local eta = etai / etat
 	local k = 1 - eta ^ 2 * (1 - cosi ^ 2)
 	return k < 0 and nullVec or eta * d + (eta * cosi - math.sqrt(k)) * n
 end
@@ -77,12 +81,9 @@ function tracer:bsdf(material, depth)
 		local ior = self:ior(self.object)
 		local eta = cosi > 0 and 1 / ior or ior
 		local fr = self:fresnel(cosi, eta)
-		local r = self:reflect(self.d, self.n)
-		local t = self:refract(self.d, self.n, cosi, ior)
-		local bias = params.glass_bias
-		local dist = params.ray_dist
-		local tr = Ray.new(self.x + t * bias, t * dist)
-		local rr = Ray.new(self.x + r * bias, r * dist)
+		local r, t = self:reflect(self.d, self.n), self:refract(self.d, self.n, cosi, ior)
+		local bias, dist = params.glass_bias,params.ray_dist
+		local rr, tr = Ray.new(self.x + r * bias, r * dist), Ray.new(self.x + t * bias, t * dist)
 		return self.albedo * (self:trace(rr, depth) * fr + self:trace(tr, depth) * (1 - fr))
 	end
 end
